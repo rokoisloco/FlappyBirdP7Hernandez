0087c00dcb1ea5e2198dbd830b57d01558c8922d
Byron Hernandez <739744@student.dusd.net>
2023-12-13 12:19:52 -0800

    Update 1.6
    Stopped at video 9/10 13:27 in the video.

--- FlappyBirdP7Hernandez/Assets/Scripts/ColumnPool.cs
+++ FlappyBirdP7Hernandez/Assets/Scripts/ColumnPool.cs
@@ -8,10 +8,14 @@ public class ColumnPool : MonoBehaviour
     public int columnPoolSize = 5;
     public GameObject columnPrefab;
     public float spawnRate = 4f;
+    public float columnMin = -1f;
+    public float columnMax = 3.5f;
 
     private GameObject[] columns;
     private Vector2 objectPoolPosition = new Vector2(-15f, -25f);
     private float timeSinceLastSpawned;
+    private float spawnXPosition = 10f;
+    private int currentColumn = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -30,7 +34,14 @@ public class ColumnPool : MonoBehaviour
 
         if (GameControl.instance.gameOver == false && timeSinceLastSpawned > spawnRate)
         {
-
+            timeSinceLastSpawned = 0;
+            float spawnYPosition = Random.Range (columnMin , columnMax);
+            columns[currentColumn].transform.position = new Vector2 (spawnXPosition, spawnYPosition);
+            currentColumn++;
+            if (currentColumn >= columnPoolSize)
+            {
+                currentColumn = 0;
+            }
         }
     }
 }
